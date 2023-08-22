861bd3a70f76f071f0bf5b53a91766f677950352
Prajwal P <prajwalprakash3722@gmail.com>
2023-08-22 12:31:57 +0530

    updated nvim to accept github co-pilot suggestions

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Keymaps are automatically loaded on the VeryLazy event
+
+-- making github co-pilot suggestions accept with shift + enter
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-e>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
